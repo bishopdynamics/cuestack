601f2cb0d940c40d2ed8d630cba21745dc3c1820
James Bishop <james@bishopdynamics.com>
2021-04-05 17:35:07 -0700

    added nuitka build script

---
+++ CueStack/build.bat
@@ -0,0 +1,14 @@
+rmdir /S /Q dist
+mkdir dist
+
+py -m nuitka --mingw64 --onefile --windows-onefile-tempdir VoicemeeterAgent.py
+rmdir /S /Q VoicemeeterAgent.build
+rmdir /S /Q VoicemeeterAgent.dist
+rmdir /S /Q VoicemeeterAgent.onefile-build
+move VoicemeeterAgent.exe .\dist\
+
+py -m nuitka --mingw64 --onefile --windows-onefile-tempdir CueStack.py
+rmdir /S /Q CueStack.build
+rmdir /S /Q CueStack.dist
+rmdir /S /Q CueStack.onefile-build
+move Cuestack.exe .\dist\

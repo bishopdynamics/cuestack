2adb2725c1bc5a5c56c7f3ccc4fd691f3befa4b5
James Bishop <james@bishopdynamics.com>
2021-04-08 15:39:52 -0700

    still tryna build

--- AudioTrigger/build.bat
+++ AudioTrigger/build.bat
@@ -7,8 +7,8 @@ rmdir /S /Q "%CWD%\dist"
 mkdir "%CWD%\dist"
 
 
-CALL npm install -g nexe
-CALL npm install
+::CALL npm install -g nexe
+::CALL npm install
 cd "%CWD%\src
 CALL nexe index.js -t x64-14.15.3 -r "public" -r "../node_modules"
 

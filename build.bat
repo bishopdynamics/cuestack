723aa946dad17337c970548422ab12ee8375567f
James Bishop <james@bishopdynamics.com>
2021-04-08 13:35:05 -0700

    tweak

--- build.bat
+++ build.bat
@@ -6,16 +6,16 @@ echo "current working directory: %CWD%"
 rmdir /S /Q "%CWD%\dist"
 mkdir "%CWD%\dist"
 
-cd "%CWD%\CueStack"
-build
-move "%CWD%\CueStack\dist\*" %CWD%\dist\
-rmdir /S /Q "%CWD%\CueStack\dist"
-cd "%CWD%"
-
 cd "%CWD%\CueStackClient"
 build
 move "%CWD%\CueStackClient\dist\CueStackClient.exe" "%CWD%\dist\"
 rmdir /S /Q "%CWD%\CueStackClient\dist"
+cd "%CWD%"
+
+cd "%CWD%\CueStack"
+build
+move "%CWD%\CueStack\dist\*" "%CWD%\dist\"
+rmdir /S /Q "%CWD%\CueStack\dist"
 cd "%CWD%"
 
 echo "done"

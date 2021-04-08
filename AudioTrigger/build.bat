436b17579744d1f6836a1803e91fb8fee41ec364
James Bishop <james@bishopdynamics.com>
2021-04-08 15:31:22 -0700

    undo aud reorg

--- AudioTrigger/build.bat
+++ AudioTrigger/build.bat
@@ -6,10 +6,12 @@ echo current working directory: %CWD%
 rmdir /S /Q "%CWD%\dist"
 mkdir "%CWD%\dist"
 
+
 CALL npm install -g nexe
 CALL npm install
-CALL nexe index.js -t x64-14.15.3 -r "public" -r "node_modules"
+cd "%CWD%\src
+CALL nexe index.js -t x64-14.15.3 -r "public" -r "../node_modules"
 
-move AudioTrigger.exe .\dist\
+move AudioTrigger.exe "%CWD%\dist\"
 
 echo Finished building AudioTrigger.exe

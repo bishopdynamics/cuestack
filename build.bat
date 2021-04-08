6f473e862608325ea17bded96445058523ad681b
James Bishop <james@bishopdynamics.com>
2021-04-08 13:28:24 -0700

    try building everything in one go

---
+++ build.bat
@@ -0,0 +1,21 @@
+
+@echo off
+set CWD = %~dp0
+echo "current working directory: %CWD%"
+
+rmdir /S /Q %CWD%\dist
+mkdir %CWD%\dist
+
+cd %CWD%\CueStack
+build
+move %CWD%\CueStack\dist\* %CWD%\dist\
+rmdir /S /Q %CWD%\CueStack\dist
+cd %CWD%
+
+cd %CWD%\CueStackClient
+build
+move %CWD%\CueStackClient\dist\CueStackClient.exe %CWD%\dist\
+rmdir /S /Q %CWD%\CueStackClient\dist
+cd %CWD%
+
+echo "done"

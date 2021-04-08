78ac8be6e6500c64f8030811bf2be5cfafd7abc5
James Bishop <james@bishopdynamics.com>
2021-04-08 13:10:09 -0700

    include files in client

--- CueStackClient/build.bat
+++ CueStackClient/build.bat
@@ -1,7 +1,7 @@
 rmdir /S /Q dist
 mkdir dist
 
-py -m nuitka --mingw64 --onefile --plugin-enable=pylint-warnings --windows-onefile-tempdir CueStackClient.py
+py -m nuitka --mingw64 --onefile --plugin-enable=pylint-warnings --windows-onefile-tempdir --include-data-file=javascript.js=javascript.js --include-data-file=index.html=index.html CueStackClient.py
 rmdir /S /Q CueStackClient.build
 rmdir /S /Q CueStackClient.dist
 rmdir /S /Q CueStackClient.onefile-build

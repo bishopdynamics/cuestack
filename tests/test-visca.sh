63d1744794dcebf50ae2c6477ec833d4dfa211ab
James Bishop <james@bishopdynamics.com>
2021-04-19 02:14:26 -0700

    add visca test

---
+++ tests/test-visca.sh
@@ -0,0 +1,11 @@
+PRESET_LIST="0 1 2 3"
+REST_TIME="0.1"
+
+while true; do 
+	for NUM in $PRESET_LIST; do
+		echo -n "calling preset ${NUM}: "
+		curl 'http://localhost:8080/trigger?cue=test_visca_preset_'"$NUM"
+		echo ""
+		sleep "$REST_TIME"
+	done
+done
